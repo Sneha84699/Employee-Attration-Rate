09b93e2c7e0b7e31eaed061536c819247fdd0e9b
Sneha84699 <sneha84699@gmail.com>
2024-08-08 18:41:38 +0530

    Update app_main.py

--- app_main.py
+++ app_main.py
@@ -16,11 +16,11 @@ st.header("")
 col1, col2 = st.columns(2)
 
 with col1:
-el=st.slider("Education Level", 1,5,2)
-tos = st.slider("Time of Service", 1.0, 100.0, 0.5)
-top = st.slider("Time of Promotion", 0.0, 50.0, 0.5)
-gr = st.slider("Growth Rate", 1,5,2)
-gr1 = st.slider("Post Level", 1,5,2)
+    el=st.slider("Education Level", 1,5,2)
+    tos = st.slider("Time of Service", 1.0, 100.0, 0.5)
+    top = st.slider("Time of Promotion", 0.0, 50.0, 0.5)
+    gr = st.slider("Growth Rate", 1,5,2)
+    gr1 = st.slider("Post Level", 1,5,2)
 
 st.text('')
 if st.button("Chance To Get Admission"):
